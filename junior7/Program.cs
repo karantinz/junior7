3d374cbd83dcb307bde9217c9d4b7ee9300959ad
Zmeegolov <karantinz@mail.ru>
2023-03-31 18:07:32 +0300

    Избавился от маг чисел

--- junior7/Program.cs
+++ junior7/Program.cs
@@ -1,12 +1,14 @@
-﻿int peopleCount;
+﻿int minutesPerPerson = 10;
+int minutesInHour = 60;
+int peopleCount;
 int minutes;
 int hours;
 
 Console.Write("Введите количество людей в очереди: ");
 peopleCount = Convert.ToInt32(Console.ReadLine());
 
-minutes = peopleCount * 10;
-hours = minutes / 60;
-minutes = minutes % 60;
+minutes = peopleCount * minutesPerPerson;
+hours = minutes / minutesInHour;
+minutes %= minutesInHour;
 
 Console.WriteLine($"Вы должны отстоять в очереди {hours} час и {minutes} минут");
